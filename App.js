53ca545cce063e924ab82c3769c9890f0f499b0d
GFR04 <gugarez123@gmail.com>
2025-04-30 21:15:27 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native'; // Adicionei 'Image' na importação
+import LogoImagem from './assets/icon1.png'; // Importando corretamente a imagem
+import { useFonts } from '@expo-google-fonts/montserrat/useFonts';
+import { Montserrat_100Thin } from '@expo-google-fonts/montserrat/100Thin';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.logo} source={LogoImagem} /> 
+      <Text style={styles.text}>Inflassometro</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#161616',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 16,
+    alignItems: 'center',
+    fontFamily: "Montserrat_100Thin",
+  },
+  logo: {
+    height: 64, // Ajustei as dimensões para algo mais visível
+    width: 64,  // Ajustei as dimensões para algo mais visível
   },
 });
